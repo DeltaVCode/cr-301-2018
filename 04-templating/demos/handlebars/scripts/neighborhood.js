90cdb2e4712b8189d991f6278d7e1205a51f20f5
Keith Dahlby <keith@newbo.co>
2018-07-12 12:30:51 -0500

    Add Day 4 Handlbars & arrow functions

--- 04-templating/demos/handlebars/scripts/neighborhood.js
+++ 04-templating/demos/handlebars/scripts/neighborhood.js
@@ -4,7 +4,7 @@ let neighborhoods = [];
 
 // REVIEW: This is another way to use a constructor to duplicate an array of raw data objects
 function Neighborhood (rawDataObject) {
-  for (key in rawDataObject) {
+  for (let key in rawDataObject) {
     this[key] = rawDataObject[key];
   }
 };
@@ -15,13 +15,64 @@ Neighborhood.prototype.toHtml = function() {
   // 1. Get the template from the HTML document
   // 2. Use Handlebars to "compile" the HTML
   // 3. Do not forget to return the HTML from this method
+  let templateHtml = $('#neighborhood-template').text();
+
+  // renderTemplate is a function from a neighborhood to HTML
+  let renderTemplate = Handlebars.compile(templateHtml);
+
+  console.log(renderTemplate({ name: 'Keith' }));
+
+  // return renderWithoutHandlebars(this);
+  return renderTemplate(this);
 };
+
+// Compiling with Handlebars produces a function like this:
+function renderWithoutHandlebars(neighborhood) {
+  return `<h2>${neighborhood.name}</h2>`;
+}
 
 // TODO: refactor to arrow functions
 neighborhoodDataSet.forEach(function(neighborhoodObject) {
   neighborhoods.push(new Neighborhood(neighborhoodObject));
 });
 
-neighborhoods.forEach(function(ourNewNeighborhoodObject){
+neighborhoods.forEach(ourNewNeighborhoodObject => {
   $('#neighborhoods').append(ourNewNeighborhoodObject.toHtml());
 });
+
+// C#   (params) => { ... }
+// Java (params) -> { ... }
+
+/* Arrow function variations:
+No parameters:  () => { ... }
+Solo params:    x  => { ... }
+Many params:    (x, y) => { ... }
+
+Instead of { ... }, you can use a single expression:
+
+x => x * 2
+
+is same as
+
+x => { return x * 2; }
+
+
+neighborhood => neighborhood.name
+
+*/
+
+/* Arrow functions treat 'this' differently
+
+DO THIS:
+$('a').click(function () {
+  let $link = $(this)
+  console.log($link.attr('href'));
+});
+
+THIS DOES NOT WORK:
+$('a').click(() => {
+  let $link = $(this)
+  console.log($link.attr('href'));
+});
+
+*/
