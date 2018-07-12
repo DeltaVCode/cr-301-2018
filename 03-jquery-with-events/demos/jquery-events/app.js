e283eadd7d42fd4c65468fc806b9a09ad7cd4de0
Keith Dahlby <keith@newbo.co>
2018-07-12 11:20:16 -0500

    Day 4 effects demo

--- 03-jquery-with-events/demos/jquery-events/app.js
+++ 03-jquery-with-events/demos/jquery-events/app.js
@@ -22,30 +22,52 @@ $(document).ready(function() {
   // Hide Tab Content At Start
   $(".tab-content").hide();
 
+  var tabCount = $(".tab-content").length; // number
+
   // Show correct tab on nav click
   $("nav a").on("click", function() {
     //Hide Tab content again so that we don't show everything if click both tabs
-    $(".tab-content").hide();
+    //$(".tab-content").hide();
+    
+    //SlideUp tab content rather than just hide
+    //$(".tab-content").slideUp();
+    
+    //FadeOut tab content rather that just hide
+    $(".tab-content").fadeOut();
+    
+    // Slide toggle all tab-content sections
+    //$(".tab-content").slideToggle();
 
     //Grab value of data-tab from clicked link
-    let $activeTab = $(this).data("tab");
+    let $clickedTab = $(this);
+    let activeTab = $clickedTab.data("tab");
 
     //Find ELement with ID of activeTab and show it
-    $("#" + $activeTab).show();
+    //$("#" + activeTab).show();
+
+    //SlideDown tab content
+    //$("#" + activeTab).slideDown();
+
+    //Fade In tab content
+    $("#" + activeTab).fadeIn();
 
   })
   
   //Select Box Image - OnChange handler
   $("select[name='icecream']").on("change", function() {
     // Get flavor name by grabbing value of selected option
-    let $flavor = $(this).val();
+    let flavor = $(this).val();
+
     $("img").hide();
     //Show image based on data-flavor attribute
-    $("img[data-flavor='" + $flavor + "']").show();
+    // $("img[data-flavor='" + flavor + "']").show();
+    $(`img[data-flavor='${flavor}']`).show();
   })
 
   // Menu item cloned and appended on click 
-  $('button[name="adder1"]').on('click', function() {
+  $('button[name="adder1"]').on('click', function(event) {
+    event.preventDefault();
+
     // Grab and clone first menu item
     let $newMenu1 = $('#menu1 li:first').clone();
 
@@ -57,6 +79,18 @@ $(document).ready(function() {
   $('input[name="check"]').on('change', function() {
     let $checkbox = $(this);
     $("#checked-state").html(".attr('checked'): " + $checkbox.attr('checked') + "<br />.prop('checked'): " + $checkbox.prop('checked'));
+  })
+
+  //Animate
+  $(".trigger-animate").on("click", function() {
+    $(".animated-box").animate({
+      left: "+=50",
+      top: "+=20",
+      opacity: "0.5"
+    }, 2500, function() {
+      //Animation Complete
+      console.log("finished!");
+    });
   })
 })
 
