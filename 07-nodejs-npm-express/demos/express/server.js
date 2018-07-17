591d5619e32bbc9d7de8f1634c75d4b15af7d33b
Keith Dahlby <keith@newbo.co>
2018-07-17 10:42:10 -0500

    Day 7 demo after npm init

---
+++ 07-nodejs-npm-express/demos/express/server.js
@@ -0,0 +1,18 @@
+console.log('Hello from server.js via nodemon!')
+
+// Load Express
+
+// Instantiate Express so that we can use its functionality
+
+// Designate a port to serve our app on
+
+// Define which directory we will serve files from
+
+// Here is one way to set up a route... bats...
+
+// Here is our sneaky secret 'weasels' route...
+
+
+// Let's 404 everything except for our routes and index.html
+
+// Now let's tell the app to listen so that it can do its thing
